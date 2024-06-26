b8c797d23203b29ecae6d3baff3a2f5a43cf4526
Tim Niklas Uhl <uhl@kit.edu>
2024-06-26 23:38:23 +0200

    Scripts for docker

---
+++ 00_pull_docker_image.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+docker pull ghcr.io/kamping-site/kamping-reproducibility:main
---
+++ 01_run_docker_container_here.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+docker run -it --rm -v $(pwd):/home/kamping ghcr.io/kamping-site/kamping-reproducibility:main
