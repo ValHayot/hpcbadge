64129a129771ef63088c4c0fac57725bbfbde53e
Tim Niklas Uhl <uhl@kit.edu>
2024-06-28 12:39:08 +0200

    A4: use GCC-12.

--- artifact4/01_build_benchmark.sh
+++ artifact4/01_build_benchmark.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
 
-cmake -S kamping-label-propagation -B kamping-label-propagation/build -DCMAKE_BUILD_TYPE=Release
+cmake -S kamping-label-propagation \
+    -B kamping-label-propagation/build \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_COMPILER=g++-12 \
+    -DCMAKE_C_COMPILER=gcc-12
+
 cmake --build kamping-label-propagation/build --parallel
